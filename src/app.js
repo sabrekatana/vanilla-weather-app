720da6a925b308ccf8ed6d2889c2edf310a407a5
sabrerodriguez <sabre.rodriguez@kidstrong.com>
2023-09-13 17:35:39 -0700

    API call for searched city

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-let apiKey = "48eb25c4a02543a3ta93fefffob47a4f";
-let apiUrlCurrent =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}";
 let apiUrlCord =
   "https://api.shecodes.io/weather/v1/current?lon={lon}&lat={lat}&key={key}";
 let apiUrlForecast =
@@ -8,6 +5,28 @@ let apiUrlForecast =
 let apiUrlCorecastCord =
   "https://api.shecodes.io/weather/v1/forecast?lon={lon}&lat={lat}&key={key}";
 
+//video has function to convert date time lesson 6
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return ` ${hours}:${minutes}`;
+}
+
+//function to display data in current forecast card
 function displayCurrent(response) {
   let city = document.querySelector("#city");
   city.innerHTML = response.data.city;
@@ -22,9 +41,20 @@ function displayCurrent(response) {
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.temperature.humidity;
   let localTime = document.querySelector("#current-time");
-  localTime.innerHTML = response.data.time;
+  localTime.innerHTML = formatDate(response.data.time * 1000);
 }
 
-axios
-  .get("https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}")
-  .then(displayCurrent);
+function search(city) {
+  let apiKey = "48eb25c4a02543a3ta93fefffob47a4f";
+  let apiUrlCurrent = `https://api.shecodes.io/weather/v1/current?query=${city}}&key=${apiKey}`;
+  axios.get(apiUrlCurrent).then(displayCurrent);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  search(city.value);
+}
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", handleSearch);
